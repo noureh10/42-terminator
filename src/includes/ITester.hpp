326c85384ed454c42579ca124f40a03ba73251a7
noureh10 <noureh.it@outlook.com>
2024-07-19 17:20:09 +0200

    1 - Initial commit, setting up a basic interface and a main file

---
+++ src/includes/ITester.hpp
@@ -0,0 +1,18 @@
+#ifndef ITESTER_HPP
+# define ITESTER_HPP
+
+# include <iostream>
+# include <string>
+
+class ITester
+{
+
+	public:
+		virtual ~ITester();
+		
+
+	private:
+
+};
+
+#endif /* ********************************************************* ITESTER_H */
