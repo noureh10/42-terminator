576529e8296d5f98781b1eed4f93525cb7af5131
noureh10 <noureh.it@outlook.com>
2024-07-22 18:53:42 +0200

    10 - Updating the push swap header file with the necessary functions for the checker as well as the attributes

--- src/includes/PushSwapChecker.hpp
+++ src/includes/PushSwapChecker.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   PushSwapChecker.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
+/*   By: nechaara <nechaara.student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 02:04:53 by nechaara          #+#    #+#             */
-/*   Updated: 2024/07/21 18:22:59 by nechaara         ###   ########.fr       */
+/*   Updated: 2024/07/22 18:51:58 by nechaara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,29 @@
 # include <iostream>
 # include <string>
 
+typedef struct s_stack
+{
+	struct s_stack	*nxt;
+	struct s_stack	*prv;
+}	t_stack;
+
 class PushSwapChecker : public ITester
 {
+	private:
+		t_stack *a;
+		t_stack *b;
+		bool	isStackSorted(t_stack *head);
+		t_stack	*initStack(std::string input);
+		void	pa(t_stack *b, t_stack *a);
+		void	pb(t_stack *a, t_stack *b);
+		void	ro(t_stack *stack);
+		void	rr(t_stack *stack);
+		void	sw(t_stack *stack);
 	public:
 		void initialize() override;
 		void runTests() override;
 		void cleanup() override;
-		std::string getResults() const override;
+		void getResults() const override;
 };
 
 #endif
